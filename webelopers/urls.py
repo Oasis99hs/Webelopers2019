a9a1bbc5aafc976ec1c8603b9e325131feffe67d
Oasis99hs <hoseinsobhi@gmail.com>
2019-11-07 15:20:25 +0330

    10

--- webelopers/urls.py
+++ webelopers/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 app_name = 'webelopers'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('registered/', register, name='registered'),
 ]
--- webelopers/views.py
+++ webelopers/views.py
@@ -1,8 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
+from .models import *
 
 
 # Create your views here.
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+
+class RegisterView(generic.TemplateView):
+    template_name = 'register.html'
+
+
+def register(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+    user = User(first_name, last_name, username, email, password1)
+    print(first_name)
+    print(last_name)
+    print(username)
+    print(email)
+    print(password1)
+    user.save()
+    return HttpResponseRedirect(reverse('webelopers:register'))
