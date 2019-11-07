0ebb458b53882410c87f29c726da07110d36c64d
Oasis99hs <hoseinsobhi@gmail.com>
2019-11-07 12:32:48 +0330

    p1

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!A:\Programming\Webelopers2019\WebelopersContest2019\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
